39370232dd07faf17c4838e41462360aedbe4a48
Mohammad Hossein Khorrami <mohammadkhoram3@gmail.com>
2020-07-14 10:58:20 +0430

    showAlertDialog()

--- app/src/main/java/com/example/testnewaptasessions/MainActivity.java
+++ app/src/main/java/com/example/testnewaptasessions/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.testnewaptasessions;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -28,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                progressDialog.dismiss(); 
+                progressDialog.dismiss();
             }
         },progress);
         progressDialog.setProgressStyle(progressDialog.STYLE_HORIZONTAL);
@@ -44,5 +47,25 @@ public class MainActivity extends AppCompatActivity {
             }
         },0,progress);
         progressDialog.show();
+    }
+
+    public void showAlert(View view){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("Alert Dialog")
+                .setMessage("Do you want to delete this file?")
+                .setCancelable(false)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,"files deleted",Toast.LENGTH_LONG).show();
+                    }
+                })
+                .setNegativeButton("No",null)
+                .setNeutralButton("`Cancel`",null)
+                .show();
+
+
+
     }
 }
